bd7a11ff4996c9bd35718a67793e57fe4cf6891c
Eugene Obrezkov <ghaiklor@gmail.com>
2015-06-18 18:45:46 +0300

    Remove Mandrill mailer

--- test/MandrillMailer.test.js
+++
@@ -1,8 +0,0 @@
-var assert = require('chai').assert;
-var MandrillMailer = require('../lib/MandrillMailer');
-
-describe('MandrillMailer', function () {
-  it('Should properly export', function () {
-    assert.isFunction(MandrillMailer);
-  });
-});
