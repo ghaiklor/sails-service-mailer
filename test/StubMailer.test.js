e9af964e7818c6eecbfb3a5b8a8e7b4eafa30bc9
Eugene Obrezkov <ghaiklor@gmail.com>
2015-06-19 11:41:14 +0300

    Done with tests for StubMailer

--- test/StubMailer.test.js
+++ test/StubMailer.test.js
@@ -1,18 +1,47 @@
 var assert = require('chai').assert;
-var StubMailer = require('../lib/BaseMailer');
+var StubMailer = require('../lib/StubMailer');
 
 describe('StubMailer', function () {
   it('Should properly export', function () {
     assert.isFunction(StubMailer);
   });
 
-  it('Should properly send message', function (done) {
-    var mailer = new StubMailer();
+  it('Should properly send message with pre-defined options', function (done) {
+    var mailer = new StubMailer({
+      from: 'some@mail.com',
+      to: 'ghaiklor@gmail.com',
+      subject: 'Hello',
+      text: 'Hello, World!'
+    });
 
     mailer
       .send()
       .then(function (result) {
-        console.log(result);
+        assert.equal(result.envelope.from, 'some@mail.com');
+        assert.equal(result.envelope.to, 'ghaiklor@gmail.com');
+        assert.ok(/Subject: Hello/.test(result.response.toString()));
+        assert.ok(/Hello, World!/.test(result.response.toString()));
+        done();
+      })
+      .catch(done);
+  });
+
+  it('Should properly override pre-defined options', function (done) {
+    var mailer = new StubMailer({
+      from: 'some@mail.com',
+      subject: 'Hello',
+      text: 'Hello, world!'
+    });
+
+    mailer
+      .send({
+        to: ['ghaiklor@gmail.com', 'another@mail.com']
+      })
+      .then(function (result) {
+        assert.equal(result.envelope.from, 'some@mail.com');
+        assert.deepEqual(result.envelope.to, ['ghaiklor@gmail.com', 'another@mail.com']);
+        assert.ok(/Subject: Hello/.test(result.response.toString()));
+        assert.ok(/Hello, world!/.test(result.response.toString()));
         done();
       })
       .catch(done);
