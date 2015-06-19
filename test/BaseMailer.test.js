ca01e41f3b0c0dc2af5803d31a28740fbd2dca28
Eugene Obrezkov <ghaiklor@gmail.com>
2015-06-19 11:27:42 +0300

    Improve test for base configurable

--- test/BaseMailer.test.js
+++ test/BaseMailer.test.js
@@ -28,12 +28,16 @@ describe('BaseMailer', function () {
 
     assert.notOk(mailer.getConfig('foo'));
     assert.instanceOf(mailer.setConfig('foo', 'bar'), BaseMailer);
+    assert.instanceOf(mailer.setConfig('transporter', {foo: 'bar'}), BaseMailer);
+    assert.deepEqual(mailer.getConfig('transporter'), {foo: 'bar'});
+    assert.equal(mailer.getConfig('transporter').foo, 'bar');
     assert.equal(mailer.getConfig('foo'), 'bar');
   });
 
   it('Should properly create mailer with pre-defined config', function () {
     var mailer = new BaseMailer({
       from: 'ghaiklor@gmail.com',
+      to: 'ghaiklor@gmail.com',
       transporter: {
         bar: 'foo',
         foo: {
@@ -43,6 +47,7 @@ describe('BaseMailer', function () {
     });
 
     assert.equal(mailer.getConfig('from'), 'ghaiklor@gmail.com');
+    assert.equal(mailer.getConfig('to'), 'ghaiklor@gmail.com');
     assert.equal(mailer.getConfig('transporter').bar, 'foo');
     assert.deepEqual(mailer.getConfig('transporter').foo, {bar: 'foo'});
     assert.notOk(mailer.getConfig('NOT_EXISTS'));
