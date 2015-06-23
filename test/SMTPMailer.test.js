c53540b5223e1643766cae1273c6e3904273ebb3
Eugene Obrezkov <ghaiklor@gmail.com>
2015-06-23 10:56:24 +0300

    Add coverage for smtp mailer

--- test/SMTPMailer.test.js
+++ test/SMTPMailer.test.js
@@ -1,4 +1,6 @@
 var assert = require('chai').assert;
+var Promise = require('bluebird');
+var sinon = require('sinon');
 var SMTPMailer = require('../lib/SMTPMailer');
 
 describe('SMTPMailer', function () {
@@ -6,9 +8,34 @@ describe('SMTPMailer', function () {
     assert.isFunction(SMTPMailer);
   });
 
-  it('Should properly send message', function () {
+  it('Should properly instantiate', function () {
+    var mailer = new SMTPMailer();
+    assert.instanceOf(mailer, SMTPMailer);
+  });
+
+  it('Should properly send mail', function (done) {
     var mailer = new SMTPMailer({
-      transporter: {}
+      from: 'no-reply@ghaiklor.com'
     });
+
+    sinon.stub(mailer._transporter, 'sendMail', function (config, cb) {
+      cb();
+    });
+
+    mailer
+      .send({
+        to: 'another@mail.com'
+      })
+      .then(function () {
+        assert(mailer._transporter.sendMail.calledOnce);
+        assert.equal(mailer._transporter.sendMail.getCall(0).args[0].from, 'no-reply@ghaiklor.com');
+        assert.equal(mailer._transporter.sendMail.getCall(0).args[0].to, 'another@mail.com');
+        assert.isFunction(mailer._transporter.sendMail.getCall(0).args[1]);
+
+        mailer._transporter.sendMail.restore();
+
+        done();
+      })
+      .catch(done);
   });
 });
