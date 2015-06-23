c65d99933600a0581ee1f366fabbbecbaca638e7
Eugene Obrezkov <ghaiklor@gmail.com>
2015-06-23 10:36:48 +0300

    Cover direct mailer with stub fn

--- test/DirectMailer.test.js
+++ test/DirectMailer.test.js
@@ -14,16 +14,28 @@ describe('DirectMailer', function () {
   });
 
   it('Should properly send mail', function (done) {
-    var sendMailStub = sinon.stub().returns(Promise.resolve());
     var mailer = new DirectMailer({
       from: 'no-reply@ghaiklor.com'
     });
 
-    mailer._transporter.sendMail = sendMailStub;
+    sinon.stub(mailer._transporter, 'sendMail', function (config, cb) {
+      cb();
+    });
 
     mailer
-      .send()
-      .then(done)
+      .send({
+        to: 'another@mail.com'
+      })
+      .then(function () {
+        assert(mailer._transporter.sendMail.calledOnce);
+        assert.equal(mailer._transporter.sendMail.getCall(0).args[0].from, 'no-reply@ghaiklor.com');
+        assert.equal(mailer._transporter.sendMail.getCall(0).args[0].to, 'another@mail.com');
+        assert.isFunction(mailer._transporter.sendMail.getCall(0).args[1]);
+
+        mailer._transporter.sendMail.restore();
+
+        done();
+      })
       .catch(done);
   });
 });
