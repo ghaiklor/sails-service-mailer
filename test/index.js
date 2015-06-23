16ddc14d3c129ea841b05a8f7d00dad43553e8fe
Eugene Obrezkov <ghaiklor@gmail.com>
2015-06-23 11:28:41 +0300

    Add coverage for service

--- test/index.js
+++ test/index.js
@@ -1,9 +1,37 @@
 var assert = require('chai').assert;
 var MailerService = require('../');
+var StubMailer = require('../lib/StubMailer');
 
 describe('MailerService', function () {
   it('Should properly export', function () {
     assert.isObject(MailerService);
     assert.isFunction(MailerService.create);
+    assert.isFunction(MailerService.DirectMailer);
+    assert.isFunction(MailerService.SendGridMailer);
+    assert.isFunction(MailerService.SendMailMailer);
+    assert.isFunction(MailerService.SESMailer);
+    assert.isFunction(MailerService.SMTPMailer);
+    assert.isFunction(MailerService.StubMailer);
+  });
+
+  it('Should properly create mailer instance', function () {
+    assert.instanceOf(MailerService.create('stub'), StubMailer);
+  });
+
+  it('Should properly send mail', function (done) {
+    var stubMailer = MailerService.create('stub', {
+      from: 'no-reply@ghaiklor.com'
+    });
+
+    stubMailer
+      .send({
+        to: 'another@mail.com'
+      })
+      .then(function (result) {
+        assert.equal(result.envelope.from, 'no-reply@ghaiklor.com');
+        assert.equal(result.envelope.to, 'another@mail.com');
+        done();
+      })
+      .catch(done);
   });
 });
