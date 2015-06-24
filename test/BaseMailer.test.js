89d33e73ecd90b257e1e688eb0180012ec30e1ea
Eugene Obrezkov <ghaiklor@gmail.com>
2015-06-24 15:53:08 +0300

    Remove unused methods

--- test/BaseMailer.test.js
+++ test/BaseMailer.test.js
@@ -6,16 +6,6 @@ describe('BaseMailer', function () {
     assert.isFunction(BaseMailer);
     assert.isFunction(BaseMailer.prototype.getConfig);
     assert.isFunction(BaseMailer.prototype.setConfig);
-    assert.isFunction(BaseMailer.prototype.getFrom);
-    assert.isFunction(BaseMailer.prototype.setFrom);
-    assert.isFunction(BaseMailer.prototype.getTo);
-    assert.isFunction(BaseMailer.prototype.setTo);
-    assert.isFunction(BaseMailer.prototype.getSubject);
-    assert.isFunction(BaseMailer.prototype.setSubject);
-    assert.isFunction(BaseMailer.prototype.getText);
-    assert.isFunction(BaseMailer.prototype.setText);
-    assert.isFunction(BaseMailer.prototype.getHtml);
-    assert.isFunction(BaseMailer.prototype.setHtml);
     assert.isFunction(BaseMailer.prototype.send);
 
     assert.throw(function () {
@@ -51,41 +41,6 @@ describe('BaseMailer', function () {
     assert.equal(mailer.getConfig('transporter').bar, 'foo');
     assert.deepEqual(mailer.getConfig('transporter').foo, {bar: 'foo'});
     assert.notOk(mailer.getConfig('NOT_EXISTS'));
-  });
-
-  it('Should properly get/set from value', function () {
-    var mailer = new BaseMailer();
-    assert.notOk(mailer.getFrom());
-    assert.instanceOf(mailer.setFrom('ghaiklor@gmail.com'), BaseMailer);
-    assert.equal(mailer.getFrom(), 'ghaiklor@gmail.com');
-  });
-
-  it('Should properly get/set to value', function () {
-    var mailer = new BaseMailer();
-    assert.notOk(mailer.getTo());
-    assert.instanceOf(mailer.setTo('ghaiklor@gmail.com'), BaseMailer);
-    assert.equal(mailer.getTo(), 'ghaiklor@gmail.com');
-  });
-
-  it('Should properly get/set subject value', function () {
-    var mailer = new BaseMailer();
-    assert.notOk(mailer.getSubject());
-    assert.instanceOf(mailer.setSubject('Hey'), BaseMailer);
-    assert.equal(mailer.getSubject(), 'Hey');
-  });
-
-  it('Should properly get/set text value', function () {
-    var mailer = new BaseMailer();
-    assert.notOk(mailer.getText());
-    assert.instanceOf(mailer.setText('Message'), BaseMailer);
-    assert.equal(mailer.getText(), 'Message');
-  });
-
-  it('Should properly get/set html value', function () {
-    var mailer = new BaseMailer();
-    assert.notOk(mailer.getHtml());
-    assert.instanceOf(mailer.setHtml('<h1>Message</h1>'), BaseMailer);
-    assert.equal(mailer.getHtml(), '<h1>Message</h1>');
   });
 
   it('Should properly throw error on send', function () {
