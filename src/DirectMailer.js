0393b921f4bfd17067e58adf96ba350922a96767
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-28 10:42:01 +0300

    Rewrite DirectMailer with ES6

--- src/DirectMailer.js
+++ src/DirectMailer.js
@@ -1,35 +1,25 @@
-var util = require('util');
-var _ = require('lodash');
-var Promise = require('bluebird');
-var nodemailer = require('nodemailer');
-var directTransport = require('nodemailer-direct-transport');
-var BaseMailer = require('./BaseMailer');
+import _ from 'lodash';
+import nodemailer from 'nodemailer';
+import directTransport from 'nodemailer-direct-transport';
+import BaseMailer from './BaseMailer';
 
-util.inherits(DirectMailer, BaseMailer);
+export default class DirectMailer extends BaseMailer {
+  constructor(...args) {
+    super(...args);
 
-/**
- * Create mailer instance for direct sending mail to MX server
- * @constructor
- */
-function DirectMailer() {
-  BaseMailer.apply(this, arguments);
+    this.setProvider(nodemailer.createTransport(directTransport(this.get('provider'))));
+  }
 
-  this.setTransporter(nodemailer.createTransport(directTransport(this.get('transporter'))));
-}
+  /**
+   * Send mail
+   * @param {Object} [_config] Additional configuration for overriding
+   * @returns {Promise}
+   */
+  send(_config) {
+    let config = _.omit(_.merge({}, this.get(), _config), 'provider');
 
-/**
- * Send mail
- * @param {Object} [_config] Additional configuration for overriding
- * @returns {Promise}
- */
-DirectMailer.prototype.send = function (_config) {
-  var config = _.omit(_.merge({}, this.get(), _config), 'transporter');
-
-  return new Promise(function (resolve, reject) {
-    this.getTransporter().sendMail(config, function (error, result) {
-      return error ? reject(error) : resolve(result);
+    return new Promise((resolve, reject) => {
+      this.getProvider().sendMail(config, (error, result) => error ? reject(error) : resolve(result));
     });
-  }.bind(this));
-};
-
-module.exports = DirectMailer;
+  }
+}
